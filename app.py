096e1ce5b97892d15102884fc8f84dca58bdd983
JONATHAN ONG <211788R@mymail.nyp.edu.sg>
2022-11-12 20:11:32 +0800

    the

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 app.debug = True
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def login():
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
