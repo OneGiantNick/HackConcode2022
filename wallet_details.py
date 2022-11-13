66b148c099a386fc50cab840a1a1a91f78f2ecde
Nicholas Ong <ong.zhengkai.nicholas@dhs.sg>
2022-11-13 20:21:04 +0800

    last commit

--- wallet_details.py
+++ wallet_details.py
@@ -1,3 +1,3 @@
 # Enter wallet details here
-my_address = ""
-private_key = ""
+my_address = "0x188dc2C8965Fd04aFEDAB814eE9d0f0543EaCa95"
+private_key = "90b4b31ea9a02788459c38da508b238fcadc3d4d2669320631d6358b0ee71592"
